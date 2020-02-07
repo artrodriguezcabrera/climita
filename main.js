d840df8baa9ec8984c5b6c5b9502d35be1788b62
artrodriguezcabrera <artrodriguezcabrera@gmail.com>
2020-02-07 01:09:58 -0500

    Add MapBox functionality to add localized map image to page

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ let weatherData = null;
 let button = document.getElementById("get-location");
 let latText = document.getElementById("latitude");
 let longText = document.getElementById("longitude");
+let mapImage = document.getElementById('map');
 
 
+//Use Browser Geolocation to pull Latitude and Longitude
 button.addEventListener("click", function() {
   navigator.geolocation.getCurrentPosition(function(position) {
     latitude = position.coords.latitude;
@@ -18,10 +20,12 @@ button.addEventListener("click", function() {
     latText.innerText = `Estas localizado en ${latitude.toFixed(2)}° latitud por `;
     longText.innerText =`${longitude.toFixed(2)}° longitud.`;
     getDarkSkyData(darkSkyKey, latitude, longitude);
+    getMapBoxImage(latitude, longitude);
   });
 });
 
 
+// Make the API call to Dark Sky and receive data
 function getDarkSkyData(darkSkyKey, latitude, longitude) {
     // API URL with CORS Anywhere to avoid CORS error
     let darkSkyAPI = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${darkSkyKey}/${latitude},${longitude}?lang=es`
@@ -35,13 +39,12 @@ function getDarkSkyData(darkSkyKey, latitude, longitude) {
 
     request.onload = function() {
         weatherData = request.response;
-        // printData(weatherData);
-        console.log("hola!")
         updatePage(weatherData);
         return weatherData;
     }
 }
 
+// Update the page with the received Dark Sky Data
 function updatePage(json) {
     currentWeather = document.querySelector('#current-weather');
     currentCity = document.querySelector('#current-city');
@@ -51,8 +54,12 @@ function updatePage(json) {
 
 
     currentWeather.textContent = json.minutely.summary;
-    // currentWeather.textContent = json.currently.summary;
     currentTemp.textContent = `Temperatura: ${json.currently.temperature} ℉`;
     currentFeelsLike.textContent = `Se siente como: ${json.currently.apparentTemperature} ℉`;
     currentHumidity.textContent = `Humedad: ${parseFloat(json.currently.humidity) * 100}%`;
+}
+
+// Use MapBox to display an image map per your latitude and longitude
+function getMapBoxImage(latitude, longitude) {
+    mapImage.src = `https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${longitude},${latitude},9/297x200?access_token=pk.eyJ1IjoiYXJ0cm9kcmlndWV6IiwiYSI6ImNrNmJxYTh0MzByNWYzcHJ6cGJ3N3doZGkifQ.3F4_kRcNUf6ucC1Om0eC3Q`
 }
