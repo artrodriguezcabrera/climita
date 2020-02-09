a89b147562393b5843bdee5105f8972dbd5ddcf8
artrodriguezcabrera <artrodriguezcabrera@gmail.com>
2020-02-09 02:05:33 -0500

    Add search field and search functionality

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const darkSkyKey = '776681cd1b856d7f09336e76c87ef4e6';
 let latitude = null;
 let longitude = null;
 let weatherData = null;
+let searchData = null;
 
 let button = document.getElementById("get-location");
 let latText = document.getElementById("latitude");
@@ -20,7 +21,6 @@ button.addEventListener("click", function() {
     latText.innerText = `Estas localizado en ${latitude.toFixed(2)}° latitud por `;
     longText.innerText =`${longitude.toFixed(2)}° longitud.`;
     getDarkSkyData(darkSkyKey, latitude, longitude);
-    getMapBoxImage(latitude, longitude);
   });
 });
 
@@ -42,6 +42,8 @@ function getDarkSkyData(darkSkyKey, latitude, longitude) {
         updatePage(weatherData);
         return weatherData;
     }
+
+    getMapBoxImage(latitude, longitude);
 }
 
 // Update the page with the received Dark Sky Data
@@ -53,13 +55,44 @@ function updatePage(json) {
     currentHumidity = document.querySelector('#current-humidity');
 
 
-    currentWeather.textContent = json.minutely.summary;
-    currentTemp.textContent = `Temperatura: ${json.currently.temperature} ℉`;
-    currentFeelsLike.textContent = `Se siente como: ${json.currently.apparentTemperature} ℉`;
+    currentWeather.textContent = json.minutely ? json.minutely.summary : json.hourly.summary;
+    currentTemp.textContent = `Temperatura: ${Math.round(json.currently.temperature)} ℉`;
+    currentFeelsLike.textContent = `Se siente como: ${Math.round(json.currently.apparentTemperature)} ℉`;
     currentHumidity.textContent = `Humedad: ${parseFloat(json.currently.humidity) * 100}%`;
 }
 
 // Use MapBox to display an image map per your latitude and longitude
 function getMapBoxImage(latitude, longitude) {
     mapImage.src = `https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${longitude},${latitude},9/297x200?access_token=pk.eyJ1IjoiYXJ0cm9kcmlndWV6IiwiYSI6ImNrNmJxYTh0MzByNWYzcHJ6cGJ3N3doZGkifQ.3F4_kRcNUf6ucC1Om0eC3Q`
+}
+
+function findMyCity() {
+    let searchString = document.getElementById("mySearch").value;
+    let mapBoxCityAPI = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchString}.json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&cachebuster=1581226421146&autocomplete=true`;
+    let searchList = document.getElementById("search-results");
+
+
+    // Grab JSON data from MapBox API
+    let request = new XMLHttpRequest();
+    request.open('GET', mapBoxCityAPI);
+    request.responseType = 'json';
+    request.send();
+
+    let searchResultCities = null;
+    request.onload = function() {
+        searchData = request.response;
+        // updatePage(weatherData);
+        searchResultCities = searchData.features
+        searchResultCities.map(city => {
+            // console.log(city.place_name);
+            // console.log(searchList);
+            latitude = city.center[1];
+            longitude = city.center[0];
+            console.log(latitude, longitude);
+            // searchList.innerHTML += "<li>" + city.place_name + "</li>";
+            searchList.innerHTML += `<li onclick="getDarkSkyData(darkSkyKey, ${latitude}, ${longitude})" >${city.place_name}</li>`;
+        });
+        return searchData;
+    }
+    
 }
